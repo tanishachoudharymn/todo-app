31b7ea89a0163516343667baaa30cd167db51059
tanishachoudharymn <tanishachoudhary.mn@gmail.com>
2026-06-15 22:18:38 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from datetime import date
 
-API = "http://localhost:5000"
+API = "https://todo-app-4-1yfe.onrender.com"
 
 st.set_page_config(page_title="My To-Do List", page_icon="✨")
 
