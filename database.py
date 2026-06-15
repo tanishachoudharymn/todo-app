2a8f224698c0255010079b70673bc8b4a8303055
tanishachoudharymn <tanishachoudhary.mn@gmail.com>
2026-06-15 22:58:07 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,14 +2,26 @@ import sqlite3
 
 def init_db():
     conn = sqlite3.connect("tasks.db")
-    conn.execute("DROP TABLE IF EXISTS tasks")
+
     conn.execute("""
-        CREATE TABLE tasks (
+        CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            task TEXT NOT NULL,
-            done BOOLEAN DEFAULT 0,
-            due_date TEXT DEFAULT NULL
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL
         )
     """)
+
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            task TEXT NOT NULL,
+            done BOOLEAN DEFAULT 0,
+            due_date TEXT DEFAULT NULL,
+            FOREIGN KEY (user_id) REFERENCES users(id)
+        )
+    """)
+
     conn.commit()
     conn.close()
+
